82b03159f4876794cb05fea055bb2578ecf39cb9
lintangqurota <lintangqurota73@gmail.com>
2021-03-10 20:18:58 +0700

    pertwmuan 1

---
+++ 2.php
@@ -0,0 +1,34 @@
+<?php
+$login=true;
+$cities=[
+    [
+        'nama' => 'chicago, IL',
+        'populasi' => '2,695,598',
+    ],
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar</title>
+</head>
+<body>
+    <h1>data mahasiswa</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>nama</th>
+                <th>nim</th>
+            </tr>
+        </thead>
+        <tbody>
+            <? foreach($cities as $city) : ?>
+                <tr>
+                    <td> <</td>
+                </tr>
+        </tbody>
+    </table>
+</body>
+</html>
