3e596230f657c5403dc7f9db7d388b462d63b383
lintangqurota <lintangqurota73@gmail.com>
2021-03-10 20:14:04 +0700

    pertemuan 1

---
+++ 1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>.</title>
+</head>
+<body>
+<h1>Belajar PHP</h1>
+<?php
+
+$a = 10;
+$b = 4;
+$c = $a + $b; 
+echo "Hasil dari Tambahan $a + $b = $c"; 
+echo "</br>"; 
+echo "=======================";
+echo "</br>";
+
+$x = 7; 
+$y = 4;
+$z = $x - $y;
+echo "Hasil dari Pengurangan $x - $y = $z"; 
+?>
+</body>
+</html>
